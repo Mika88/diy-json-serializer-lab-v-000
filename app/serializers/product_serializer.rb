68115a69c4338f4a84c175a395862d194630e9c2
Mika88 <mikaspivak@gmail.com>
2019-04-17 23:53:06 +0000

    Automatically backed up by Learn

---
+++ app/serializers/product_serializer.rb
@@ -0,0 +1,10 @@
+class ProductSerializer
+  def self.serialize(product)
+    serialized_product = '{'
+    serialized_product += '"id": ' + product.id.to_s + ', '
+    serialized_product += '"name": "' + product.name + '", '
+    serialized_product += '"description": "' + product.description + '", '
+    serialized_product += '"price": "' + product.price + '", '
+    serialized_product += '}'
+  end
+end
