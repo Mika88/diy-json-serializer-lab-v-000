8ecaeb56212af2fe54ce26b015f7cf44f6e4ba41
Mika88 <mikaspivak@gmail.com>
2019-04-17 23:53:36 +0000

    Automatically backed up by Learn

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -5,6 +5,7 @@ class ProductSerializer
     serialized_product += '"name": "' + product.name + '", '
     serialized_product += '"description": "' + product.description + '", '
     serialized_product += '"price": "' + product.price + '", '
+    serialized_product += '"inventory": "' + product.inventory + '", '
     serialized_product += '}'
   end
 end
